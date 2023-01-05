d970903d64c26ac9b309e84c27602ed1909d48a1
Russ Whitchurch <russell@whitchurch.net>
2023-01-05 09:09:24 -0700

    Move alloy out of data layer

--- websdk/DataLayer.js
+++ websdk/DataLayer.js
@@ -1,12 +1,7 @@
-alloy("configure", {
-    "edgeConfigId": "a883addc-9384-449b-8ba0-bb474296e66d",
-    "orgId": "53A16ACB5CC1D3760A495C99@AdobeOrg"
-});
-
 let params = new URLSearchParams(document.location.search);
 let dQuery = params.get("d");
 
-var dataLayer = {
+let dataLayer = {
     "xdm": {
         "web": {
             "webPageDetails": {
@@ -27,5 +22,3 @@ var dataLayer = {
         }
     }
 };
-
-alloy("sendEvent", dataLayer);
